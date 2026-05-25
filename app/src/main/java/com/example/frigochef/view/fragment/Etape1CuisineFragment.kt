3c4779a8af983a606e330d50bd0e9f1d3338fd8a
Sabrina <2184053@crosemont.qc.ca>
2026-05-24 21:14:42 -0400

    construction de la grille dans le fragment étape 1

--- app/src/main/java/com/example/frigochef/view/fragment/Etape1CuisineFragment.kt
+++ app/src/main/java/com/example/frigochef/view/fragment/Etape1CuisineFragment.kt
@@ -1,11 +1,14 @@
 package com.example.frigochef.view.fragment
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.GridLayout
 import androidx.fragment.app.Fragment
 import com.example.frigochef.R
 import com.example.frigochef.databinding.FragmentEtape1QuestionnaireBinding
-
+import com.example.frigochef.databinding.ItemCarteCuisineBinding
+import com.example.frigochef.view.QuestionnaireActivity
 
 class Etape1CuisineFragment: Fragment(R.layout.fragment_etape1_questionnaire){
 
@@ -41,8 +44,43 @@ class Etape1CuisineFragment: Fragment(R.layout.fragment_etape1_questionnaire){
     }
 
     private fun construireGrille(){
+        val inflater = LayoutInflater.from(requireContext())
 
-    }
+        cuisines.forEach{ (nom, icone) ->
+
+            val carteBinding = ItemCarteCuisineBinding.inflate(
+                inflater,
+                binding.gridCuisines,
+                false
+            )
+
+            carteBinding.tvIconeCuisine.text = icone
+            carteBinding.tvNomCuisine.text   = nom
+
+            // Configurer les parametres pour le gridLayout
+            val params = GridLayout.LayoutParams().apply{
+                width       = 0
+                columnSpec  = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+                rowSpec     = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+            }
+            carteBinding.root.layoutParams = params
+
+            carteBinding.root.setOnClickListener{
+
+                if(selectionnees.contains(nom)){
+                    selectionnees.remove(nom)
+                }else{
+                    selectionnees.add(nom)
+                }
+                mettreAJourCarte(carteBinding, nom)
+                (requireActivity() as QuestionnaireActivity).cuisinesSelectionnees =
+                    selectionnees.toList()
+            }
+
+            binding.gridCuisines.addView(carteBinding.root)
+        }
+
+        }
 
     // Doit parcourir toutes les cartes et mettre a jour leurs couleurs
     private fun rafraichirAffichage(){
